f6023bdb756e286b4c3888ef4f024e5437a85faf
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2024-08-08 08:11:40 +0200

    fix history for SPA routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import {createRouter, createWebHistory} from "vue-router";
+import {createRouter, createWebHashHistory, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import LoginPage from "@/views/LoginView.vue";
 import MyOrdersView from "@/views/MyOrdersView.vue";
 import OrderSummaryView from "@/views/OrderSummaryView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
