8e62abb994a3f318e0c1065dfd47acad32908bdf
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2024-08-08 02:09:27 +0200

    fix ts issue

--- src/components/AccountSelector.vue
+++ src/components/AccountSelector.vue
@@ -1,48 +1,51 @@
 <template>
-  <div class="account-selector" @click="$emit('select', account)">
-    <span class="account-name">{{ account.name }}</span>
-    <img :src="account.picture" :alt="account.name" class="profile-picture">
-  </div>
+    <div class="account-selector" @click="$emit('select', account)">
+        <span class="account-name">{{ account.name }}</span>
+        <img :src="account.picture" :alt="account.name" class="profile-picture">
+    </div>
 </template>
 
-<script>
+<script lang="ts">
+import {type PropType} from 'vue'
+import {type Account} from "@/backend";
+
 export default {
-  name: 'AccountSelector',
-  props: {
-    account: {
-      type: Object,
-        required: true
+    name: 'AccountSelector',
+    props: {
+        account: {
+            type: Object as PropType<Account>,
+            required: true
+        }
     }
-  }
 }
 </script>
 
 <style scoped>
 .account-selector {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 10px;
-  margin: 5px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: background-color 0.3s;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
 }
 
 .account-selector:hover {
-  background-color: #f0f0f0;
+    background-color: #f0f0f0;
 }
 
 .profile-picture {
-  width: 250px;
-  height: 250px;
-  border-radius: 10%;
-  object-fit: cover;
-  margin-right: 15px;
+    width: 250px;
+    height: 250px;
+    border-radius: 10%;
+    object-fit: cover;
+    margin-right: 15px;
 }
 
 .account-name {
-  font-size: 32px;
+    font-size: 32px;
 }
 </style>
