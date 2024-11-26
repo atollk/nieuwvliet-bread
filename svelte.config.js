1ff28f941f7cc2be2cd32e96e685247ea5a2b865
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-11-26 23:33:24 +0100

    ci

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,27 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+import {preprocessMeltUI, sequence} from '@melt-ui/pp';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: sequence([
+        vitePreprocess(),
+        preprocessMeltUI(),
+    ]),
 
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/nieuwvliet-bread'
-		},
-		alias: {
-			'@': './src'
-		},
-		preprocess: sequence([preprocessMeltUI()])
-	}
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.argv.includes('dev') ? '' : '/nieuwvliet-bread'
+        },
+        alias: {
+            '@': './src'
+        },
+    }
 };
 
 export default config;
