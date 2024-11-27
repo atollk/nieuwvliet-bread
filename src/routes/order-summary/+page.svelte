74b675da8ad030d95406e330952ce62ca4cbd586
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-11-27 19:52:36 +0100

    prettier

--- src/routes/order-summary/+page.svelte
+++ src/routes/order-summary/+page.svelte
@@ -1,101 +1,110 @@
 <script lang="ts">
-    import {goto} from "$app/navigation";
-    import type {Account, OrderItem} from "@/backend/types";
-    import {getOrderData} from "@/backend/supabase";
-    import {loadAccountsFromCsv, loadItemsFromCSV} from "@/backend/static";
-    import {base} from "$app/paths";
+	import { goto } from '$app/navigation';
+	import type { Account, OrderItem } from '@/backend/types';
+	import { getOrderData } from '@/backend/supabase';
+	import { loadAccountsFromCsv, loadItemsFromCSV } from '@/backend/static';
+	import { base } from '$app/paths';
 
-    interface FetchData {
-        orderData: Map<string, number[]>
-        accounts: Account[]
-        items: OrderItem[]
-    }
+	interface FetchData {
+		orderData: Map<string, number[]>;
+		accounts: Account[];
+		items: OrderItem[];
+	}
 
-    const getFetchData = async (): Promise<FetchData> => {
-        return {
-            orderData: await getOrderData(),
-            accounts: await loadAccountsFromCsv(),
-            items: await loadItemsFromCSV(),
-        }
-    }
+	const getFetchData = async (): Promise<FetchData> => {
+		return {
+			orderData: await getOrderData(),
+			accounts: await loadAccountsFromCsv(),
+			items: await loadItemsFromCSV()
+		};
+	};
 
-    let fetchDataPromise = $state<Promise<FetchData>>(getFetchData())
-    let hoveredItem = $state<OrderItem | undefined>();
+	let fetchDataPromise = $state<Promise<FetchData>>(getFetchData());
+	let hoveredItem = $state<OrderItem | undefined>();
 
-    const getCellValue = (orderData: Map<string, number[]>, item?: OrderItem, account?: Account): number => {
-        let total = 0
-        for (const [k, v] of orderData.entries()) {
-            if (account !== undefined && account.name !== k) continue
-            if (item === undefined) {
-                total += v.reduce((a, b) => a + b, 0)
-            } else {
-                total += v[item.id - 1]
-            }
-        }
-        return total
-    }
+	const getCellValue = (
+		orderData: Map<string, number[]>,
+		item?: OrderItem,
+		account?: Account
+	): number => {
+		let total = 0;
+		for (const [k, v] of orderData.entries()) {
+			if (account !== undefined && account.name !== k) continue;
+			if (item === undefined) {
+				total += v.reduce((a, b) => a + b, 0);
+			} else {
+				total += v[item.id - 1];
+			}
+		}
+		return total;
+	};
 
-    const showImage = (item: OrderItem): void => {
-        hoveredItem = item;
-    }
+	const showImage = (item: OrderItem): void => {
+		hoveredItem = item;
+	};
 
-    const hideImage = (item: OrderItem): void => {
-        if (hoveredItem?.id === item.id)
-            hoveredItem = undefined;
-    }
+	const hideImage = (item: OrderItem): void => {
+		if (hoveredItem?.id === item.id) hoveredItem = undefined;
+	};
 </script>
 
 <div class="flex flex-col items-center gap-8">
-    <h1 class="h1 mt-8">Zusammenfassung der Bestellungen</h1>
-    {#await fetchDataPromise}
-        <img class="w-32" src="{base}/loading.gif" alt="Loading"/>
-    {:then fetchData}
-        <div class="max-w-4xl">
-            <table class="">
-                <thead>
-                <tr class="my-11">
-                    <th>Ware</th>
-                    <th>Summe</th>
-                    <th class="w-16"></th>
-                    {#each fetchData.accounts as account (account.id)}
-                        <th>{account.name}</th>
-                    {/each}
-                </tr>
-                </thead>
-                <tbody>
-                <tr class="h-2"></tr>
-                <tr>
-                    <td class="italic">Summe</td>
-                    <td class="text-center">{getCellValue(fetchData.orderData, undefined, undefined)}</td>
-                    <td></td>
-                    {#each fetchData.accounts as account (account.id)}
-                        <td class="text-center">{getCellValue(fetchData.orderData, undefined, account)}</td>
-                    {/each}
-                </tr>
-                <tr class="h-2"></tr>
-                {#each fetchData.items as item (item.id)}
-                    <tr>
-                        <td onmouseover={() => showImage(item)} onfocus={() => showImage(item)}
-                            onmouseout={() => hideImage(item)}
-                            onblur={() => hideImage(item)}>
-                            <span>{item.name}</span>
-                            {#if hoveredItem?.id === item.id}
-                                <img class="absolute w-32 h-auto shadow-2xl border-2 border-primary rounded-xl"
-                                     src={item.image} alt={item.name}>
-                            {/if}
-                        </td>
-                        <td class="text-center">{getCellValue(fetchData.orderData, item, undefined)}</td>
-                        <td></td>
-                        {#each fetchData.accounts as account (account.id)}
-                            <td class="text-center">{getCellValue(fetchData.orderData, item, account)}</td>
-                        {/each}
-                    </tr>
-                {/each}
-                </tbody>
-            </table>
-        </div>
-    {:catch error}
-        {error}
-    {/await}
-    <button class="" onclick={() => goto(`${base}/home`)}>Zurück</button>
+	<h1 class="h1 mt-8">Zusammenfassung der Bestellungen</h1>
+	{#await fetchDataPromise}
+		<img class="w-32" src="{base}/loading.gif" alt="Loading" />
+	{:then fetchData}
+		<div class="max-w-4xl">
+			<table class="">
+				<thead>
+					<tr class="my-11">
+						<th>Ware</th>
+						<th>Summe</th>
+						<th class="w-16"></th>
+						{#each fetchData.accounts as account (account.id)}
+							<th>{account.name}</th>
+						{/each}
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="h-2"></tr>
+					<tr>
+						<td class="italic">Summe</td>
+						<td class="text-center">{getCellValue(fetchData.orderData, undefined, undefined)}</td>
+						<td></td>
+						{#each fetchData.accounts as account (account.id)}
+							<td class="text-center">{getCellValue(fetchData.orderData, undefined, account)}</td>
+						{/each}
+					</tr>
+					<tr class="h-2"></tr>
+					{#each fetchData.items as item (item.id)}
+						<tr>
+							<td
+								onmouseover={() => showImage(item)}
+								onfocus={() => showImage(item)}
+								onmouseout={() => hideImage(item)}
+								onblur={() => hideImage(item)}
+							>
+								<span>{item.name}</span>
+								{#if hoveredItem?.id === item.id}
+									<img
+										class="absolute h-auto w-32 rounded-xl border-2 border-primary shadow-2xl"
+										src={item.image}
+										alt={item.name}
+									/>
+								{/if}
+							</td>
+							<td class="text-center">{getCellValue(fetchData.orderData, item, undefined)}</td>
+							<td></td>
+							{#each fetchData.accounts as account (account.id)}
+								<td class="text-center">{getCellValue(fetchData.orderData, item, account)}</td>
+							{/each}
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	{:catch error}
+		{error}
+	{/await}
+	<button class="" onclick={() => goto(`${base}/home`)}>Zurück</button>
 </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,20 @@
-import type {Config} from 'tailwindcss';
-import plugin from "tailwindcss/plugin";
-
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
+import daisyui from 'daisyui';
 
 const config = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
-		plugin(({addVariant}) => {
-			addVariant("pointer-coarse", "@media (pointer: coarse)")
-			addVariant("pointer-fine", "@media (pointer: fine)")
-			addVariant("hover-none", "@media (hover: none)")
-			addVariant("hover-hover", "@media (hover: hover)")
-		})
+		plugin(({ addVariant }) => {
+			addVariant('pointer-coarse', '@media (pointer: coarse)');
+			addVariant('pointer-fine', '@media (pointer: fine)');
+			addVariant('hover-none', '@media (hover: none)');
+			addVariant('hover-hover', '@media (hover: hover)');
+		}),
+		daisyui
 	]
 } satisfies Config;
 
