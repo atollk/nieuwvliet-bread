27cc8c49b6988f8c3d1bb4fea35f768a30331a5d
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-11-30 00:25:40 +0100

    enable prerendering

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,5 @@
-import { initializeStores } from '@skeletonlabs/skeleton';
-
 export const ssr = false;
-export const prerender = false;
+export const prerender = true;
 
 // Handle the base path for client-side routing
 export const load = ({ url }) => {
